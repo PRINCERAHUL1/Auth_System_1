792002ccbf3d6b63139f84ddc509c9bc9e4a27b8
Rahul Mandal <rmrahul258@gmail.com>
2024-01-28 16:41:54 +0530

    CreateAccountDB.php

--- CreateAccountDB.php
+++ CreateAccountDB.php
@@ -1,5 +1,5 @@
 <?php
-	//error_reporting(0);
+	error_reporting(0);
 	
 	$fname = $_POST['first'];
 	$lname = $_POST['last'];
@@ -13,16 +13,72 @@
 	$h = mysqli_connect("localhost","root","");
 	mysqli_select_db($h,"xyz");
 	
-	if($userpass == $usercon)
+	if($fname == '')
 	{
-		$q = "insert into create_acc values('".$fname."','".$lname."','".$ge."',".$phn.",'".$email."','".$userid."','".$userpass."')";
-		mysqli_query($h,$q);
-	
-		mysqli_close($h);
-		header("Location:index.php?c=1");
+		header("Location:CreateAccount.php?p=1");
 	}
 	else
 	{
-		header("Location:CreateAccount.php?d=1");
+		if($lname == '')
+		{
+			header("Location:CreateAccount.php?q=1");
+		}
+		else
+		{
+			if($ge == '')
+			{
+				header("Location:CreateAccount.php?r=1");
+			}
+			else
+			{
+				if($phn == '')
+				{
+					header("Location:CreateAccount.php?s=1");
+				}
+				else
+				{
+					if($email == '')
+					{
+						header("Location:CreateAccount.php?t=1");
+					}
+					else
+					{
+						if($userid == '')
+						{
+							header("Location:CreateAccount.php?u=1");
+						}
+						else
+						{
+							if($userpass == '')
+							{
+								header("Location:CreateAccount.php?v=1");
+							}
+							else
+							{
+								if($usercon == '')
+								{
+									header("Location:CreateAccount.php?w=1");
+								}
+								else
+								{
+									if($userpass == $usercon)
+									{
+										$q = "INSERT INTO create_acc (first_name, last_name, gender, mobile, email, user, password) VALUES ('$fname', '$lname', '$ge', $phn, '$email', '$userid', '$userpass')";
+										mysqli_query($h,$q);
+										
+										mysqli_close($h);
+										header("Location:index.php?c=1");
+									}
+									else
+									{
+										header("Location:CreateAccount.php?d=1");
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
 	}
 ?>
